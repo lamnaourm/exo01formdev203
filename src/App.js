f1f228c25faf7a144fc5cb5c3be564596b5657de
lamnaourm <mohammed.lamnaour@gmail.com>
2023-10-13 14:54:56 +0100

    les composants

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Calcul from './components/Calcul';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="container">
-      
+      <Header />
+      <Calcul />
+      <Footer />
     </div>
   );
 }
