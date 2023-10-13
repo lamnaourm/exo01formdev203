22ba282dcc82801b2e8becfdd62c5421ebf8301f
lamnaourm <mohammed.lamnaour@gmail.com>
2023-10-13 15:11:26 +0100

    style

--- src/components/Calcul.jsx
+++ src/components/Calcul.jsx
@@ -1,36 +1,37 @@
 import React, { Component } from 'react'
 
 export default class Calcul extends Component {
+    
     state = {
-        
+
     }
-  render() {
-    return (
-      <div className='calcul'>
-        <div className='form'>
-            <div className='group'>
-                <label htmlFor="capital">Capital : </label>
-                <input type="number" name="capital" id="capital" />
+
+    render() {
+        return (
+            <div className='calcul'>
+                <div className='form'>
+                    <div className='group'>
+                        <label htmlFor="capital">Capital : </label>
+                        <input type="number" name="capital" id="capital" />
+                    </div>
+                    <div className='group'>
+                        <label htmlFor="taux">Taux d'interet : </label>
+                        <input type="number" name="taux" id="taux" />
+                    </div>
+                    <div className='group'>
+                        <label htmlFor="duree">Durée : </label>
+                        <input type="number" name="duree" id="duree" />
+                    </div>
+                    <div className='btns'>
+                        <button>Calculer</button>
+                        <button>Initialiser</button>
+                    </div>
+                    <div className='group'>
+                        <label htmlFor="mensualite">Mensualité : </label>
+                        <input type="number" name="mensualite" id="mensualite" readOnly />
+                    </div>
+                </div>
             </div>
-            <div className='group'>
-                <label htmlFor="taux">Taux d'interet : </label>
-                <input type="number" name="taux" id="taux" />
-            </div>
-            <div className='group'>
-                <label htmlFor="duree">Durée : </label>
-                <input type="number" name="duree" id="duree" />
-            </div>
-            <div className='btns'>
-                <button>Calculer</button>
-                <button>Initialiser</button>
-            </div>
-            <hr />
-            <div className='group'>
-                <label htmlFor="mensualite">Mensualité : </label>
-                <input type="number" name="mensualite" id="mensualite" />
-            </div>
-        </div>
-      </div>
-    )
-  }
+        )
+    }
 }
