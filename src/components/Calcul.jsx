aaaca2fab6fe5fe9f9f5e10ecf0468466e1efc63
lamnaourm <mohammed.lamnaour@gmail.com>
2023-10-13 15:49:11 +0100

    calcul

--- src/components/Calcul.jsx
+++ src/components/Calcul.jsx
@@ -3,7 +3,35 @@ import React, { Component } from 'react'
 export default class Calcul extends Component {
     
     state = {
+        capital:0,
+        taux:0,
+        duree:0,
+        mensualite:0
+    }
 
+    handleOnChange = (e) => {
+
+        const name = e.target.name;
+        const value = e.target.value;
+
+        this.setState({[name] : value})
+    }
+
+    calucler = () => {
+
+        const mensualite = (this.state.capital * this.state.taux / 1200) / (1 - Math.pow((1+this.state.taux/1200), -this.state.duree))
+
+        this.setState({mensualite})
+
+    }
+
+    initialiser = () => {
+        this.setState({
+            capital:0,
+            taux:0,
+            duree:0,
+            mensualite:0
+        })
     }
 
     render() {
@@ -12,23 +40,23 @@ export default class Calcul extends Component {
                 <div className='form'>
                     <div className='group'>
                         <label htmlFor="capital">Capital : </label>
-                        <input type="number" name="capital" id="capital" />
+                        <input type="number" name="capital" id="capital" value={this.state.capital} onChange={this.handleOnChange}/>
                     </div>
                     <div className='group'>
                         <label htmlFor="taux">Taux d'interet : </label>
-                        <input type="number" name="taux" id="taux" />
+                        <input type="number" name="taux" id="taux" value={this.state.taux} onChange={this.handleOnChange} />
                     </div>
                     <div className='group'>
                         <label htmlFor="duree">Durée : </label>
-                        <input type="number" name="duree" id="duree" />
+                        <input type="number" name="duree" id="duree" value={this.state.duree} onChange={this.handleOnChange} />
                     </div>
                     <div className='btns'>
-                        <button>Calculer</button>
-                        <button>Initialiser</button>
+                        <button onClick={this.calucler}>Calculer</button>
+                        <button onClick={this.initialiser}>Initialiser</button>
                     </div>
                     <div className='group'>
                         <label htmlFor="mensualite">Mensualité : </label>
-                        <input type="number" name="mensualite" id="mensualite" readOnly />
+                        <input type="number" name="mensualite" id="mensualite" value={this.state.mensualite.toFixed(2)} onChange={this.handleOnChange} readOnly />
                     </div>
                 </div>
             </div>
